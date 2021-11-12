cb7fb01389fb2043a5a95d0e0f1216e5e4456311
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-12 16:58:27 -0500

    Taller del traductor terminado

---
+++ tallerArchivo/src/main/java/ejercicioTraductor/Archivo.java
@@ -0,0 +1,63 @@
+package ejercicioTraductor;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Archivo {
+    File diccionario = new File("C:\\Users\\SAR\\Desktop\\sofka\\tallerArchivo\\Diccionario.txt");
+
+    public void escribirDiccionario(HashMap Diccionario) {
+        try {
+            FileWriter escribir = new FileWriter(diccionario, true);
+            escribir.write("\r\n" +String.valueOf(Diccionario)+ ",");
+            escribir.close();
+        } catch (IOException e) {
+            System.err.println("Error, no se ha podido escribir sobre el archivo");
+        }
+    }
+
+    private String traerDatosArchivo() {
+        String linea = null;
+        String DiccionarioString = null;
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader(diccionario));
+            StringBuilder cadena = new StringBuilder();
+
+            while ((linea = lector.readLine()) != null) {
+                cadena = cadena.append(linea);
+            }
+            lector.close();
+            DiccionarioString = cadena.toString();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error, " + e);
+        } catch (IOException e) {
+            System.out.println("Error, " + e);
+        }
+
+        return DiccionarioString;
+    }
+
+    public HashMap leerDiccionario(){
+        HashMap Diccionario = new HashMap();
+        String[] separadosPorComa = traerDatosArchivo().replace("{", "").replace("}", "").replace("=", ",").split(",");
+        String[] claves = new String[(separadosPorComa.length/2)+1];
+        String[] valores = new String[(separadosPorComa.length/2)+1];
+        int clavesC = 0, valoresC = 0;
+
+        for (int i = 0; i < separadosPorComa.length; i++) {
+            if (i % 2 == 0) {
+                claves[clavesC] = separadosPorComa[i];
+                clavesC++;
+            }else{
+                valores[valoresC] = separadosPorComa[i];
+                valoresC++;
+            }
+        }
+
+        for (int i = 0; i < separadosPorComa.length/2; i++) {
+            Diccionario.put(claves[i], valores[i]);
+        }
+
+        return Diccionario;
+    }
+}
---
+++ tallerArchivo/src/main/java/ejercicioTraductor/Main.java
@@ -0,0 +1,53 @@
+package ejercicioTraductor;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Controlador traductor;
+        Scanner entrada = new Scanner(System.in);
+        String entradaEspanol, entradaIngles;
+        int opcion;
+        boolean salir = false;
+
+        while (salir == false){
+            traductor = new Controlador();
+            System.out.print("\nBIENVENIDO A EL TRADUCTOR\nA continuación estan las operaciones que puede realizar\n" +
+                    "1. Insertar una nueva palabra\n" +
+                    "2. Consultar la traducción de una palabra\n" +
+                    "3. Consultar todas las palabras del diccionario\n" +
+                    "4. Salir\n" +
+                    "Ingrese el número de lo que desea hacer: ");
+            opcion = entrada.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.print("Ingrese la palabra en español: ");
+                    entradaEspanol = entrada.next();
+                    System.out.print("Ingrese la traducción en ingles: ");
+                    entradaIngles = entrada.next();
+                    traductor.agregarPalabra(entradaEspanol, entradaIngles);
+                    entradaEspanol = null;
+                    entradaIngles = null;
+                    break;
+                case 2:
+                    System.out.print("¿Qué palabra desea traducir?: ");
+                    entradaEspanol = entrada.next();
+                    traductor.traducirPalabra(entradaEspanol);
+                    break;
+                case 3:
+                    traductor.consultarTodo();
+                    break;
+                case 4:
+                    System.out.println("Vuelva pronto");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Operación invalida");
+            }
+        }
+
+    }
+}
