32889f9145a17ee79fe7e1103fbcea7aaac791bd
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-15 18:24:40 -0500

    Se agregó la clase Bar y se ajustó la clase BoardView para agregar las barras laterales que controlan la pelota

--- Taller de javascript - MVC/main.js
+++ Taller de javascript - MVC/main.js
@@ -8,11 +8,32 @@
         this.ball = null;
     }
 
-    self.Board.protopype = {
+    self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
+        }
+    }
+})();
+
+(function(){
+    self.Bar = function(x, y, width, height, board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
+
+    self.Bar.protopype = {
+        down: function(){
+
+        },
+        up: function(){
+
         }
     }
 })();
@@ -25,12 +46,34 @@
         this.board = board;
         this.ctx = canvas.getContext("2d");
     }
+
+    self.BoardView.prototype = {
+        draw: function(){
+            for (var i = this.board.elements.length -1; i >= 0; i--) {
+                var el = this.board.elements[i];
+                draw(this.ctx, el);
+            };
+        }
+    }
+
+    function draw(ctx, element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
+    }
 })();
 
 self.addEventListener("load", main);
 
 function main(){
     var board = new Board(800, 400);
+    var bar = new Bar(20, 100, 40, 100, board);
+    var bar_2 = new Bar(735, 100, 40, 100, board);
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas, board);
+    board_view.draw();
 }
