878aae0cc2694d4a5e7c175965f3bb566b084d25
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-19 12:24:20 -0500

    Se agregó movimiento a la bola y el boton de pausa

--- Taller de javascript - MVC/main.js
+++ Taller de javascript - MVC/main.js
@@ -6,29 +6,40 @@
     this.game_over = false;
     this.bars = [];
     this.ball = null;
+    this.playing = false;
   };
 
   self.Board.prototype = {
     get elements() {
-      var elements = this.bars;
+      var elements = this.bars.map(function (bar) {
+        return bar;
+      });
       elements.push(this.ball);
       return elements;
     },
   };
 })();
 
-(function (){
-    self.Ball = function(x, y, radius, board){
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.speed_y = 0;
-        this.speed_x = 3;
-        this.board = board;
-        
-        board.ball = this;
-        this.kind = "circle";
-    }
+(function () {
+  self.Ball = function (x, y, radius, board) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed_y = 0;
+    this.speed_x = 3;
+    this.board = board;
+    this.direction = 1;
+
+    board.ball = this;
+    this.kind = "circle";
+  };
+
+  self.Ball.prototype = {
+    move: function () {
+      this.x += this.speed_x * this.direction;
+      this.y += this.speed_y * this.direction;
+    },
+  };
 })();
 
 (function () {
@@ -75,10 +86,13 @@
         draw(this.ctx, el);
       }
     },
-    play: function(){
+    play: function () {
+      if (this.board.playing) {
         this.clean();
         this.draw();
-    }
+        this.board.ball.move();
+      }
+    },
   };
 
   function draw(ctx, element) {
@@ -91,7 +105,7 @@
         ctx.arc(element.x, element.y, element.radius, 0, 7);
         ctx.fill();
         ctx.closePath();
-        break; 
+        break;
     }
   }
 })();
@@ -104,22 +118,28 @@ var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function (ev) {
-  ev.preventDefault();
   if (ev.keyCode == 38) {
+    ev.preventDefault();
     bar.up();
   } else if (ev.keyCode == 40) {
+    ev.preventDefault();
     bar.down();
   } else if (ev.keyCode == 87) {
+    ev.preventDefault();
     bar_2.up();
   } else if (ev.keyCode == 83) {
+    ev.preventDefault();
     bar_2.down();
+  } else if (ev.keyCode == 32) {
+    ev.preventDefault();
+    board.playing = !board.playing;
   }
 });
 
+board_view.draw();
 window.requestAnimationFrame(controller);
 
 function controller() {
-  
-    board_view.play();
+  board_view.play();
   window.requestAnimationFrame(controller);
 }
