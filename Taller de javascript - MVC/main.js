2b166f4b274d17ce14591422ab22f8ed9702d333
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-19 15:02:18 -0500

    Se agregó la colision de la bola con los bordes y con las barras

--- Taller de javascript - MVC/main.js
+++ Taller de javascript - MVC/main.js
@@ -26,9 +26,12 @@
     this.y = y;
     this.radius = radius;
     this.speed_y = 0;
-    this.speed_x = 3;
+    this.speed_x = 13;
     this.board = board;
     this.direction = 1;
+    this.bounce_angle = 0;
+    this.max_bounce_angle = Math.PI / 12;
+    this.speed = 3;
 
     board.ball = this;
     this.kind = "circle";
@@ -38,6 +41,42 @@
     move: function () {
       this.x += this.speed_x * this.direction;
       this.y += this.speed_y * this.direction;
+
+      if (this.y <= 8) {
+        this.speed_y = -this.speed_y;
+        this.bounce_angle = -this.bounce_angle;
+      }
+      if (this.y >= 394) {
+        this.speed_y = -this.speed_y;
+        this.bounce_angle = -this.bounce_angle;
+      }
+      if (this.x >= 800) {
+        this.board.playing = false;
+        alert("Fin del juego, recarga la página si quieres volver a jugar");
+      }
+      if (this.x <= 0) {
+        this.board.playing = false;
+        alert("Fin del juego, recarga la página si quieres volver a jugar");
+      }
+    },
+    get width() {
+      return this.radius * 2;
+    },
+    get height() {
+      return this.radius * 2;
+    },
+    collision: function (bar) {
+      //Reacciona a la colisión con una barra que recibe como parámetro
+      var relative_intersect_y = bar.y + bar.height / 2 - this.y;
+      var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+      this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+      this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+      this.speed_X = this.speed * Math.cos(this.bounce_angle);
+
+      if (this.x > this.board.width / 2) this.direction = -1;
+      else this.direction = 1;
     },
   };
 })();
@@ -51,7 +90,7 @@
     this.board = board;
     this.board.bars.push(this);
     this.kind = "rectangle";
-    this.speed = 20;
+    this.speed = 30;
   };
 
   self.Bar.prototype = {
@@ -86,14 +125,48 @@
         draw(this.ctx, el);
       }
     },
+    check_collisions: function () {
+      for (var i = this.board.bars.length - 1; i >= 0; i--) {
+        var bar = this.board.bars[i];
+        if (hit(bar, this.board.ball)) {
+          this.board.ball.collision(bar);
+        }
+      }
+    },
     play: function () {
       if (this.board.playing) {
         this.clean();
         this.draw();
+        this.check_collisions();
         this.board.ball.move();
       }
     },
   };
+
+  function hit(a, b) {
+    //Revisa si a colisiona con b
+    var hit = false;
+    //Colisiones horizontales
+    if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+      //Colisiones verticales
+      if (b.y + b.height >= a.y && b.y < a.y + a.height) {
+        hit = true;
+      }
+    }
+    //Colision de a con b
+    if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+      if (b.y <= a.y && b.y + b.height >= a.y + a.height) {
+        hit = true;
+      }
+    }
+    //Colision b con a
+    if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+      if (a.y <= b.y && a.y + a.height >= b.y + b.height) {
+        hit = true;
+      }
+    }
+    return hit;
+  }
 
   function draw(ctx, element) {
     switch (element.kind) {
@@ -111,11 +184,11 @@
 })();
 
 var board = new Board(800, 400);
-var bar = new Bar(20, 100, 40, 100, board);
-var bar_2 = new Bar(735, 100, 40, 100, board);
+var bar = new Bar(0, 130, 30, 100, board);
+var bar_2 = new Bar(770, 130, 30, 100, board);
 var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas, board);
-var ball = new Ball(350, 100, 10, board);
+var ball = new Ball(400, 180, 10, board);
 
 document.addEventListener("keydown", function (ev) {
   if (ev.keyCode == 38) {
@@ -134,6 +207,7 @@ document.addEventListener("keydown", function (ev) {
     ev.preventDefault();
     board.playing = !board.playing;
   }
+  console.log(bar.toString());
 });
 
 board_view.draw();
