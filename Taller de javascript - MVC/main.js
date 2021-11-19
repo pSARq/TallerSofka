0c097280407250294978d65776abefcfdb8927eb
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-19 11:18:11 -0500

    Se agregó funciones down y up a la clase Bar

--- Taller de javascript - MVC/main.js
+++ Taller de javascript - MVC/main.js
@@ -1,79 +1,93 @@
-(function(){
-    self.Board = function(width, height){
-        this.width = width;
-        this.height = height;
-        this.playingm = false;
-        this.game_over = false;
-        this.bars = [];
-        this.ball = null;
-    }
+(function () {
+  self.Board = function (width, height) {
+    this.width = width;
+    this.height = height;
+    this.playingm = false;
+    this.game_over = false;
+    this.bars = [];
+    this.ball = null;
+  };
 
-    self.Board.prototype = {
-        get elements(){
-            var elements = this.bars;
-            elements.push(this.ball);
-            return elements;
-        }
-    }
+  self.Board.prototype = {
+    get elements() {
+      var elements = this.bars;
+      elements.push(this.ball);
+      return elements;
+    },
+  };
 })();
 
-(function(){
-    self.Bar = function(x, y, width, height, board){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.board = board;
-        this.board.bars.push(this);
-        this.kind = "rectangle";
+(function () {
+  self.Bar = function (x, y, width, height, board) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.board = board;
+    this.board.bars.push(this);
+    this.kind = "rectangle";
+    this.speed = 10;
+  };
+
+  self.Bar.prototype = {
+    down: function () {
+      this.y += this.speed;
+    },
+    up: function () {
+      this.y -= this.speed;
+    },
+    toString: function(){
+        return "x: "+ this.x +" y: "+ this.y;
     }
-
-    self.Bar.protopype = {
-        down: function(){
-
-        },
-        up: function(){
-
-        }
-    }
+  }
 })();
 
-(function(){
-    self.BoardView = function(canvas, board){
-        this.canvas = canvas;
-        this.canvas.width = board.width;
-        this.canvas.height = board.height;
-        this.board = board;
-        this.ctx = canvas.getContext("2d");
-    }
+(function () {
+  self.BoardView = function (canvas, board) {
+    this.canvas = canvas;
+    this.canvas.width = board.width;
+    this.canvas.height = board.height;
+    this.board = board;
+    this.ctx = canvas.getContext("2d");
+  };
 
-    self.BoardView.prototype = {
-        draw: function(){
-            for (var i = this.board.elements.length -1; i >= 0; i--) {
-                var el = this.board.elements[i];
-                draw(this.ctx, el);
-            };
-        }
-    }
+  self.BoardView.prototype = {
+    draw: function () {
+      for (var i = this.board.elements.length - 1; i >= 0; i--) {
+        var el = this.board.elements[i];
+        draw(this.ctx, el);
+      }
+    },
+  };
 
-    function draw(ctx, element){
-        if(element !== null && element.hasOwnProperty("kind")){
-            switch(element.kind){
-                case "rectangle":
-                    ctx.fillRect(element.x, element.y, element.width, element.height);
-                    break;
-            }
-        }
+  function draw(ctx, element) {
+    if (element !== null && element.hasOwnProperty("kind")) {
+      switch (element.kind) {
+        case "rectangle":
+          ctx.fillRect(element.x, element.y, element.width, element.height);
+          break;
+      }
     }
+  }
 })();
+
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 100, board);
+var bar_2 = new Bar(735, 100, 40, 100, board);
+var canvas = document.getElementById("canvas");
+var board_view = new BoardView(canvas, board);
+
+document.addEventListener("keydown", function (ev) {
+  if (ev.keyCode == 38) {
+    bar.up();
+  } else if (ev.keyCode == 40) {
+    bar.down();
+  }
+  console.log(bar.toString());
+});
 
 self.addEventListener("load", main);
 
-function main(){
-    var board = new Board(800, 400);
-    var bar = new Bar(20, 100, 40, 100, board);
-    var bar_2 = new Bar(735, 100, 40, 100, board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas, board);
-    board_view.draw();
+function main() {
+  board_view.draw();
 }
