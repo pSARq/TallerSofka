29578231b5f54721a8b337b90311a27a2d20c71f
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-28 18:18:54 -0500

    Se agrego eventsAction a Components

---
+++ kata-crud-refactoring/front/src/Components/eventsAction.jsx
@@ -0,0 +1,50 @@
+export const TYPE = {
+  TASK_CREATE: "item.TASK_CREATE",
+  TASK_UPDATE: "item.TASK_UPDATE",
+  TASK_FIND: "item.TASK_FIND",
+  TASK_DELETE: "item.TASK_DELETE",
+  TASK_ON_EDIT: "item.TASK_ON_EDIT",
+  LIST_CREATE: "list.LIST_CREATE",
+  LIST_FIND: "list.LIST_FIND",
+  LIST_DELETE: "list.LIST_DELETE",
+};
+
+export default {
+  saveTask: (idList, item) => ({
+    type: TYPE.TASK_CREATE,
+    item,
+    idList,
+  }),
+  deleteTask: (idList, itemId) => ({
+    type: TYPE.TASK_DELETE,
+    itemId,
+    idList,
+  }),
+  updateTask: (idList, item) => ({
+    type: TYPE.TASK_UPDATE,
+    item,
+    idList,
+  }),
+  onEdited: (idList, item) => ({
+    type: TYPE.TASK_ON_EDIT,
+    item,
+    idList,
+  }),
+  findTask: (idList, items) => ({
+    type: TYPE.TASK_FIND,
+    items,
+    idList,
+  }),
+  saveList: (item) => ({
+    type: TYPE.LIST_CREATE,
+    item,
+  }),
+  findList: (list) => ({
+    type: TYPE.LIST_FIND,
+    list,
+  }),
+  deleteList: (listId) => ({
+    type: TYPE.LIST_DELETE,
+    listId,
+  }),
+};
