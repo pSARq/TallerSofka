820562d763a37d2bfa78ba4a0bb93813ba734248
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-28 18:36:57 -0500

    Se agregó ListView a list

---
+++ kata-crud-refactoring/front/src/Components/list/ListView.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useContext, useEffect } from 'react';
+import ToDoForm from "../todo/FormView.jsx";
+import ToDoList from "../todo/ListView.jsx";
+import ApiList from "./ApiList";
+import events from "../eventsAction";
+import Store from "../store"
+
+function ListView(){
+    const { state: { list, todo }, dispatch } = useContext(Store);
+    const [isLoaded, setLoaded] = useState(false);
+    useEffect(() => {
+        ApiList.findAll().then((response) => {
+            if(response.ok) {
+                response.json().then((list) => {
+                    dispatch(events.findList(list));
+                });
+            }
+            setLoaded(true);
+        })
+    }, [dispatch]);
+
+    const onDelete = (listId) => {
+        ApiList.delete(listId).then((response) => {
+            if(response.ok) {
+                dispatch(events.deleteList(listId));
+            }
+        })
+    };
+
+    return <div>
+        {!isLoaded && <div>Loading</div>}
+        {list.elements.length === 0 && <div>empty list!</div>}
+        {list.elements.map((element) => {
+            return <div key={element.id} id={"list-to-do-"+element.id}>
+                <fieldset>
+                    <legend>
+                        {element.name.toUpperCase()}
+                        <button onClick={() => onDelete(element.id)}>Eliminar</button>
+                    </legend>
+                    <ToDoForm listId={element.id} todo={todo} />
+                    <ToDoList listId={element.id} todo={todo} />
+                </fieldset>
+            </div>
+        })}
+    </div>
+}
+
+export default ListView
