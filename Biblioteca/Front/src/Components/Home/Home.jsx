b009bdd5c9f36647fd6e287d861f44db3a742475
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-29 11:58:19 -0500

    Se inicializa el Home con el Navbar

---
+++ Biblioteca/Front/src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../Navbar/Navbar"
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <p>Soy el Home</p>
+        </div>
+    )
+}
+
+export default Home
