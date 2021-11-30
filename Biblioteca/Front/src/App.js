2c11fe25aa2b8be8dac69da06aa0e70001b70439
SAR <santiagoalmendrarojas19@gmail.com>
2021-11-30 12:45:02 -0500

    Se aplicó el store en App

--- Biblioteca/Front/src/App.js
+++ Biblioteca/Front/src/App.js
@@ -1,26 +1,24 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Login from "./pages/Register/Login"
-import Register from "./pages/Register/Register"
-import Navbar from "./Components/Navbar/Navbar";
+import Login from "./pages/Register/Login";
+import Register from "./pages/Register/Register";
+import { StoreProvider } from "./store";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <StoreProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </StoreProvider>
   );
 }
 
--- Biblioteca/Front/src/store.jsx
+++ Biblioteca/Front/src/store.jsx
@@ -1,24 +1,25 @@
-import React, {useReducer, createContext} from "react"
-import { reducerActions } from "./reducerActions"
+import React, { useReducer, createContext } from "react";
+import { reducerActions } from "./reducerActions";
 
-const initialState ={
-    users: [],
-    books: []
-}
+const initialState = {
+  users: [],
+  books: [],
+};
 
-const store = createContext(initialState)
+const store = createContext(initialState);
 
-function reducer(state, action){
-    console.log("dispatch => ", action.type);
-    return reducerActions()[action.type] ? reducerActions()[action.type](state, action) : state;
+function reducer(state, action) {
+  console.log("dispatch => ", action.type);
+  return reducerActions()[action.type]
+    ? reducerActions()[action.type](state, action)
+    : state;
 }
 
 export const StoreProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    return (
-      <store.Provider value={{ state, dispatch }}>{children}</store.Provider>
-    );
-  };
-  
-  export default store;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <store.Provider value={{ state, dispatch }}>{children}</store.Provider>
+  );
+};
 
+export default store;
