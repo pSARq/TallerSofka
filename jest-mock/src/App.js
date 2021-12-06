7b251dd87e03ff4cce6d08f88b64889d7aa2985c
SAR <santiagoalmendrarojas19@gmail.com>
2021-12-06 17:37:40 -0500

    Se agregaron funciones a App.js

--- jest-mock/src/App.js
+++ jest-mock/src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import * as math from "./math.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const doAdd = (a, b) => math.add(a, b)
+export const doSubtract = (a, b) => math.subtract(a, b)
+export const doMultiply = (a, b) => math.doMultiply(a, b)
+export const doDivide = (a, b) => math.doDivide(a, b)
