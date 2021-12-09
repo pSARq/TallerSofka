6b5b0e928922d657ca72afff5a03f5cad2a01cf5
SAR <santiagoalmendrarojas19@gmail.com>
2021-12-09 15:28:14 -0500

    Empezando taller

---
+++ Spring TestPublisher/Spring-TestPublisher/src/main/java/com/example/Spring/TestPublisher/SpringTestPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.TestPublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestPublisherApplication.class, args);
+	}
+
+}
---
+++ Spring TestPublisher/Spring-TestPublisher/src/test/java/com/example/Spring/TestPublisher/SpringTestPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.TestPublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
