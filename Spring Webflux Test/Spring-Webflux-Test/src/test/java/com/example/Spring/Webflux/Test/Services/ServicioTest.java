31b2dc50ad00414bc7984577f41fcbf68f79dd3b
SAR <santiagoalmendrarojas19@gmail.com>
2021-12-09 14:08:39 -0500

    Se agregaron Test

---
+++ Spring Webflux Test/Spring-Webflux-Test/src/test/java/com/example/Spring/Webflux/Test/Services/ServicioTest.java
@@ -0,0 +1,43 @@
+package com.example.Spring.Webflux.Test.Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+@SpringBootTest
+public class ServicioTest {
+    @Autowired
+    Servicio servicio;
+
+    @Test
+    void testMono(){
+        Mono<String> uno = servicio.buscarUno();
+        StepVerifier.create(uno).expectNext("Pedro").verifyComplete();
+    }
+
+    @Test
+    void testVarios(){
+        Flux<String> uno = servicio.buscarTodos();
+        StepVerifier.create(uno).expectNext("Pedro").expectNext("Maria").expectNext("Jesus").expectNext("Carmen").verifyComplete();
+    }
+
+    @Test
+    void testVariosLento(){
+        Flux<String> uno = servicio.buscarTodosLento();
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Maria")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Jesus")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Carmen")
+                .thenAwait(Duration.ofSeconds(1))
+                .verifyComplete();
+    }
+}
